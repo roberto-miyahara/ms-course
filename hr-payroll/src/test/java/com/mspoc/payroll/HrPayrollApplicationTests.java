9fe5540206f0208c48b7112b5bb56982980a4cb3
Roberto Miyahara <r.miyahara@hotmail.com>
2021-05-26 17:41:24 -0300

    configuring eureka client

--- hr-payroll/src/test/java/com/mspoc/payroll/HrPayrollApplicationTests.java
+++ hr-payroll/src/test/java/com/mspoc/payroll/HrPayrollApplicationTests.java
@@ -2,6 +2,8 @@ package com.mspoc.payroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootTest
 class HrPayrollApplicationTests {
