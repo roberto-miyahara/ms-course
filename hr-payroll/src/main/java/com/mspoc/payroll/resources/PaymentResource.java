bdc03c5a1695b55b0091fff1e24a010b269483b8
Roberto Miyahara <r.miyahara@hotmail.com>
2021-05-26 21:37:29 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/mspoc/payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/mspoc/payroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.mspoc.payroll.resources;
 
 import com.mspoc.payroll.entities.Payment;
 import com.mspoc.payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
+
 
 }
--- hr-worker/src/main/java/com/mspoc/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/mspoc/hrworker/HrWorkerApplication.java
@@ -11,5 +11,4 @@ public class HrWorkerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
-
 }
