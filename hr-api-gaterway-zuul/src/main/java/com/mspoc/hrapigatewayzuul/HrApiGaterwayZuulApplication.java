d758555e53481ed9bfba750523f6515d605344b6
Roberto Miyahara <r.miyahara@hotmail.com>
2021-05-26 21:40:42 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gaterway-zuul/src/main/java/com/mspoc/hrapigatewayzuul/HrApiGaterwayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.mspoc.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGaterwayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGaterwayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-gaterway-zuul/src/test/java/com/mspoc/hrapigatewayzuul/HrApiGaterwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspoc.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGaterwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
