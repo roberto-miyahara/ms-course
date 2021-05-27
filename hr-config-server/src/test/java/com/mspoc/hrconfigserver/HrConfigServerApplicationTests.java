173d054a9442a43f2138a8e7038862b5e49c33e0
Roberto Miyahara <r.miyahara@hotmail.com>
2021-05-27 09:29:58 -0300

    Project hr-config-server Created

---
+++ hr-config-server/src/test/java/com/mspoc/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspoc.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
