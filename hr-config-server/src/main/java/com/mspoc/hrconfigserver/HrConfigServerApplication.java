32bd91a6620d58eaeb13b8cceee6f92e7e1fa2d3
Roberto Miyahara <r.miyahara@hotmail.com>
2021-05-27 11:29:01 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/mspoc/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/mspoc/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,24 @@
 package com.mspoc.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("USERAME = " + username);
+	}
 }
